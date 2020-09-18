9ccbb9a8e4df6be0d5e130ba4c8537db951d483e
Baron-569 <70834686+Baron-569@users.noreply.github.com>
2020-09-18 10:51:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ chmod +x ts
 get() {
 rm -fr Tshake.lua
 rm -fr sudo.lua
-wget "https://raw.githubusercontent.com/TEAMTshakeX/TshAkEx/master/Tshake.lua"
+wget "https://raw.githubusercontent.com/Baron-569/Spark/master/Tshake.lua"
 lua start.lua
 }
 installall(){
